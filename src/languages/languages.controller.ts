92405f1eed4f62089ebc5cc5232d6e7a72da0816
quantumsheep <nathanael.dmc@outlook.fr>
2021-06-23 01:13:09 +0200

    Add required permissions for languages routes

--- src/languages/languages.controller.ts
+++ src/languages/languages.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { Permissions } from 'src/users/guards/decorators/permissions.decorator';
 import { CreateLanguageBodyDTO } from './dto/create-language-body.dto';
 import { CreateLanguageResponseDTO } from './dto/create-language-response.dto';
 import { GetLanguageParamsDTO } from './dto/get-language-params.dto';
@@ -37,8 +38,8 @@ export class LanguagesController {
   }
 
   @ApiBearerAuth()
-  @UseGuards(AuthGuard)
   @Post()
+  @Permissions(['manage-languages'])
   async createLanguage(
     @Body() body: CreateLanguageBodyDTO,
   ): Promise<CreateLanguageResponseDTO> {
@@ -48,8 +49,8 @@ export class LanguagesController {
   }
 
   @ApiBearerAuth()
-  @UseGuards(AuthGuard)
   @Put(':language_id')
+  @Permissions(['manage-languages'])
   async updateLanguage(
     @Param() params: UpdateLanguageParamsDTO,
     @Body() body: UpdateLanguageBodyDTO,
