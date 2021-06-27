010a573b2862973c02c6c3a94e4013f021db7773
Quentin Caritey <quentin.caritey@neoxia.com>
2021-06-27 16:42:58 +0200

    fix lint

--- src/quality/grammars/go/rules/too-many-loops.go-rule.ts
+++ src/quality/grammars/go/rules/too-many-loops.go-rule.ts
@@ -3,22 +3,26 @@ import { GoRule } from './go-rule';
 import { ForStmtContext } from '../runtime/GoParser';
 
 export const goTooManyLoopsRule: GoRule = {
-  errorMessage:
-    'Too many for loops imbrications.',
+  errorMessage: 'Too many for loops imbrications.',
   hooks: [
     {
       type: 'visitForStmt',
       test: (context: ForStmtContext) => {
         let count = 0;
         let currentContext = context;
-        while(!!currentContext) {
-          currentContext = currentContext.block().statementList().statement().find(s => !!s.forStmt())?.forStmt();
+        while (!!currentContext) {
+          currentContext = currentContext
+            .block()
+            .statementList()
+            .statement()
+            .find((s) => !!s.forStmt())
+            ?.forStmt();
           count++;
         }
         if (count >= 3) {
           return [];
         }
-      }
-    }
-  ]
+      },
+    },
+  ],
 };
