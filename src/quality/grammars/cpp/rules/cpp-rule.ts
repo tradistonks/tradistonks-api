80f7de7831b2e0fe91698b37370766462b1dff8f
Quentin Caritey <quentin.caritey@neoxia.com>
2021-07-06 18:53:49 +0200

    interfacing fixes

--- src/quality/grammars/cpp/rules/cpp-rule.ts
+++ src/quality/grammars/cpp/rules/cpp-rule.ts
@@ -1,11 +1,4 @@
-import { RuleContext } from 'antlr4ts';
-
 import { Rule } from 'src/quality/utils/rule';
 import { CPP14ParserVisitor } from '../runtime/CPP14ParserVisitor';
 
-export interface CppRule extends Rule {
-  hooks: {
-    type: keyof CPP14ParserVisitor<unknown>;
-    test: (context: RuleContext) => string[] | void;
-  }[];
-}
+export interface CppRule extends Rule<keyof CPP14ParserVisitor<unknown>> {}
